90fd166193f9d0c00bb4cf5309177109e95603b4
Gert-Jan Tromp <gerrit.jan.tromp@nxp.com>
2023-02-22 10:40:59 +0100

    Initial setup

---
+++ training_lib/blink/RTL/blink_rtl.sv
@@ -0,0 +1,26 @@
+module blink (
+    input logic clk50m,
+    input logic reset_n,
+    output logic led
+);
+
+logic [27:0] counter;
+
+always @(posedge clk50m or negedge reset_n) begin
+    if (~reset_n)
+    begin
+        counter <= 0;
+	led <= 1'b0;
+    end
+    else
+    begin
+        if (counter == 0) begin
+            counter <= 24999999;
+            led <= ~led;
+        end else begin
+            counter <= counter - 1;
+        end
+    end
+end
+
+endmodule : blink
